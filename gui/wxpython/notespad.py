43ed594a0aed03c3e45d07cd5a74246352907d48
Yoriz <dave.wilson1@ntlworld.com>
2019-06-14 00:00:23 +0100

    notespad menu undo, cut, copy, paste and delete

--- gui/wxpython/notespad.py
+++ gui/wxpython/notespad.py
@@ -14,10 +14,10 @@ class Notespad(wx.Frame):
         super(Notespad, self).__init__(*args, **kwargs)
         self.path = None
         self.frame_settings()
-        self.create_menu()
+        self.create_menus()
         self.create_gui_items()
         self.create_sizers()
-        self.create_binds()
+        self.add_binds()
  
     def frame_settings(self):
         self.update_title()
@@ -25,9 +25,14 @@ class Notespad(wx.Frame):
         self.SetMinSize((600, 400))
         self.CentreOnScreen()
  
-    def create_menu(self):
-        menubar = wx.MenuBar()
-        self.SetMenuBar(menubar)
+    def create_menus(self):
+        self.menubar = wx.MenuBar()
+        self.SetMenuBar(self.menubar)
+        self.create_file_menu()
+        self.create_edit_menu()
+        self.create_help_menu()
+ 
+    def create_file_menu(self):
         file_menu = wx.Menu()
         self.menu_new = file_menu.Append(wx.ID_NEW, '&New',
                                     'Creates a new document')
@@ -39,12 +44,28 @@ class Notespad(wx.Frame):
                                 'Saves the active document with a new name')
         file_menu.AppendSeparator()
         self.menu_exit = file_menu.Append(-1, 'E&xit', 'Exit the Application')
-        menubar.Append(file_menu, '&File')
-         
+        self.menubar.Append(file_menu, '&File')
+ 
+    def create_edit_menu(self):
+        edit_menu = wx.Menu()
+        self.menu_undo = edit_menu.Append(wx.ID_UNDO, '&Undo',
+                                    'Undo change')
+        edit_menu.AppendSeparator()
+        self.menu_cut = edit_menu.Append(wx.ID_CUT, 'Cu&t',
+                                    'Cut the selected text')
+        self.menu_copy = edit_menu.Append(wx.ID_COPY, '&Copy',
+                                    'Copy the selected text')
+        self.menu_paste = edit_menu.Append(wx.ID_PASTE, '&Paste',
+                                    'Paste from the clipboard')
+        self.menu_delete = edit_menu.Append(wx.ID_DELETE, 'De&lete',
+                                    'Delete selected text')
+        self.menubar.Append(edit_menu, '&Edit')
+ 
+    def create_help_menu(self):
         help_menu = wx.Menu()
         self.menu_help = help_menu.Append(wx.ID_HELP, '&Help',
                                          'Get Help from python-forum.io')
-        menubar.Append(help_menu, '&Help')
+        self.menubar.Append(help_menu, '&Help')
  
     def create_gui_items(self):
         self.frame_panel = wx.Panel(self)
@@ -60,15 +81,40 @@ class Notespad(wx.Frame):
         self.SetSizer(sizer)
         self.Layout()
  
-    def create_binds(self):
+    def add_binds(self):
+        self.add_frame_binds()
+        self.add_fle_menu_binds()
+        self.add_edit_menu_binds()
+        self.add_help_menu_binds()
+ 
+    def add_frame_binds(self):
+        self.Bind(wx.EVT_CLOSE, self.on_close_evt)
+ 
+    def add_fle_menu_binds(self):
         self.Bind(wx.EVT_MENU, self.on_menu_new, self.menu_new)
         self.Bind(wx.EVT_MENU, self.on_menu_open, self.menu_open)
         self.Bind(wx.EVT_MENU, self.on_menu_save, self.menu_save)
         self.Bind(wx.EVT_MENU, self.on_menu_saveas, self.menu_saveas)
-        self.Bind(wx.EVT_MENU, self.on_menu_help, self.menu_help)
         self.Bind(wx.EVT_MENU, self.on_menu_exit, self.menu_exit)
  
-        self.Bind(wx.EVT_CLOSE, self.on_close_evt)
+    def add_edit_menu_binds(self):
+        self.Bind(wx.EVT_MENU, self.on_menu_undo, self.menu_undo)
+        self.Bind(wx.EVT_UPDATE_UI, self.on_menu_undo_update, self.menu_undo)
+        self.Bind(wx.EVT_MENU, self.on_menu_cut, self.menu_cut)
+        self.Bind(wx.EVT_UPDATE_UI, self.on_menu_cut_update, self.menu_cut)
+        self.Bind(wx.EVT_MENU, self.on_menu_copy, self.menu_copy)
+        self.Bind(wx.EVT_UPDATE_UI, self.on_menu_copy_update, self.menu_copy)
+        self.Bind(wx.EVT_MENU, self.on_menu_paste, self.menu_paste)
+        self.Bind(wx.EVT_UPDATE_UI, self.on_menu_paste_update, self.menu_paste)
+        self.Bind(wx.EVT_MENU, self.on_menu_delete, self.menu_delete)
+        self.Bind(
+            wx.EVT_UPDATE_UI, self.on_menu_delete_update, self.menu_delete)
+ 
+    def add_help_menu_binds(self):
+        self.Bind(wx.EVT_MENU, self.on_menu_help, self.menu_help)
+ 
+    def on_menu_exit(self, event):
+        self.Close()
  
     def on_menu_new(self, event):
         if self.save_if_modified():
@@ -77,18 +123,45 @@ class Notespad(wx.Frame):
     def on_menu_open(self, event):
         if self.save_if_modified():
             self.open()
-             
+ 
     def on_menu_save(self, event):
         self.save()
-         
+ 
     def on_menu_saveas(self, event):
         self.save_as()
-         
+ 
+    def on_menu_undo(self, event):
+        self.txt_ctrl.Undo()
+ 
+    def on_menu_undo_update(self, event):
+        event.Enable(self.txt_ctrl.CanUndo())
+ 
+    def on_menu_cut(self, event):
+        self.txt_ctrl.Cut()
+ 
+    def on_menu_cut_update(self, event):
+        event.Enable(self.txt_ctrl.CanCut())
+ 
+    def on_menu_copy(self, event):
+        self.txt_ctrl.Copy()
+ 
+    def on_menu_copy_update(self, event):
+        event.Enable(self.txt_ctrl.CanCopy())
+ 
+    def on_menu_paste(self, event):
+        self.txt_ctrl.Paste()
+ 
+    def on_menu_paste_update(self, event):
+        event.Enable(self.txt_ctrl.CanPaste())
+ 
+    def on_menu_delete(self, event):
+        self.txt_ctrl.Remove(*self.txt_ctrl.GetSelection())
+ 
+    def on_menu_delete_update(self, event):
+        event.Enable(bool(self.txt_ctrl.GetStringSelection()))
+ 
     def on_menu_help(self, event):
         self.open_forum_page()
- 
-    def on_menu_exit(self, event):
-        self.Close()
  
     def on_close_evt(self, event):
         if event.CanVeto():
