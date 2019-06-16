6cfbbd4fdddc6d04fbe43e01358479f4c360283b
Yoriz <dave.wilson1@ntlworld.com>
2019-06-16 09:37:01 +0100

    submit_to_pool_executor will now raise errors

--- gui/tkinter/threading_example.py
+++ gui/tkinter/threading_example.py
@@ -2,34 +2,43 @@ import tkinter as tk
 from concurrent import futures
 import time
 import functools
- 
+
 thread_pool_executor = futures.ThreadPoolExecutor(max_workers=1)
- 
- 
+
+
 def tk_after(target):
- 
+
     @functools.wraps(target)
     def wrapper(self, *args, **kwargs):
         args = (self,) + args
         self.after(0, target, *args, **kwargs)
- 
+
     return wrapper
- 
- 
+
+
 def submit_to_pool_executor(executor):
- 
+    '''Decorates a method to be sumbited to the passed in executor'''
     def decorator(target):
- 
+
         @functools.wraps(target)
         def wrapper(*args, **kwargs):
-            return executor.submit(target, *args, **kwargs)
- 
+            result = executor.submit(target, *args, **kwargs)
+            result.add_done_callback(executor_done_call_back)
+            return result
+
         return wrapper
- 
+
     return decorator
- 
+
+
+def executor_done_call_back(future):
+    exception = future.exception()
+    if exception:
+        raise exception
+
+
 class MainFrame(tk.Frame):
- 
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.label = tk.Label(self, text='not running')
@@ -40,31 +49,31 @@ class MainFrame(tk.Frame):
             self, text='blocking task', command=self.on_button)
         self.button.pack(pady=15)
         self.pack()
- 
+
     def on_button(self):
         print('Button clicked')
         self.blocking_code()
- 
+
     @tk_after
     def set_label_text(self, text=''):
         self.label['text'] = text
- 
+
     @tk_after
     def listbox_insert(self, item):
         self.listbox.insert(tk.END, item)
         print(item)
- 
+
     @submit_to_pool_executor(thread_pool_executor)
     def blocking_code(self):
         self.set_label_text('running')
- 
+
         for number in range(5):
             self.listbox_insert(number)
             time.sleep(1)
- 
+
         self.set_label_text('not running')
- 
- 
+
+
 if __name__ == '__main__':
     app = tk.Tk()
     main_frame = MainFrame()
--- gui/wxpython/threading_example.py
+++ gui/wxpython/threading_example.py
@@ -2,35 +2,43 @@ import wx
 from concurrent import futures
 import time
 import functools
- 
+
 thread_pool_executor = futures.ThreadPoolExecutor(max_workers=1)
- 
- 
+
+
 def wx_call_after(target):
- 
+
     @functools.wraps(target)
     def wrapper(self, *args, **kwargs):
         args = (self,) + args
         wx.CallAfter(target, *args, **kwargs)
- 
+
     return wrapper
- 
- 
+
+
 def submit_to_pool_executor(executor):
- 
+    '''Decorates a method to be sumbited to the passed in executor'''
     def decorator(target):
- 
+
         @functools.wraps(target)
         def wrapper(*args, **kwargs):
-            return executor.submit(target, *args, **kwargs)
- 
+            result = executor.submit(target, *args, **kwargs)
+            result.add_done_callback(executor_done_call_back)
+            return result
+
         return wrapper
- 
+
     return decorator
- 
- 
+
+
+def executor_done_call_back(future):
+    exception = future.exception()
+    if exception:
+        raise exception
+
+
 class MainFrame(wx.Frame):
- 
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.panel = wx.Panel(self)
@@ -45,31 +53,31 @@ class MainFrame(wx.Frame):
         self.panel_sizer.Add(self.button, 0, wx.ALL | wx.CENTER, 5)
         self.Layout()
         self.Show()
- 
+
     def on_button(self, event):
         print('Button clicked')
         self.blocking_code()
- 
+
     @wx_call_after
     def set_label_text(self, text=''):
         self.label.SetLabel(text)
- 
+
     @wx_call_after
     def listbox_insert(self, item):
         self.listbox.Append(item)
         print(item)
- 
+
     @submit_to_pool_executor(thread_pool_executor)
     def blocking_code(self):
         self.set_label_text('running')
- 
+
         for number in range(5):
             self.listbox_insert(str(number))
             time.sleep(1)
- 
+
         self.set_label_text('not running')
- 
- 
+
+
 if __name__ == '__main__':
     app = wx.App(False)
     main_frame = MainFrame(None)
